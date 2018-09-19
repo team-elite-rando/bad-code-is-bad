bbc2cab06c346051cc436d0d8e73d0ee1240898e
Steve Moats <steven.moats@gmail.com>
2018-09-18 20:59:39 -0400

    Add files via upload
    Simplest way I could come up with.

---
+++ level-two/challenge-1/solved/steve.js
@@ -0,0 +1,13 @@
+
+// This codes goal: Simply take the array of animals and add an s to the end of each one
+
+// ==== DO NOT EDIT BETWEEN THESE LINES
+const animals = ['Lion', 'Zebra', 'Tiger', 'Duck']
+// ==== Okay you can edit again
+
+const results = animals.map(a => `${a}s`)
+
+// ==== DO NOT EDIT BETWEEN THESE LINES
+// Output expected: ['Lions', 'Zebras', 'Tigers', 'Ducks']
+console.log(results)
+// ==== Okay you can edit again
